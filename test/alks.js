5ebe05c5fd9ba32ca1510a634dbbb45567234ff8
Brian <brian.antonelli@gmail.com>
2016-09-09 12:42:18 -0400

    test stub for travis

---
+++ test/alks.js
@@ -0,0 +1,11 @@
+/*jslint node: true */
+'use strict';
+
+var mashery = require('../lib/keys');
+var assert = require('chai').assert;
+
+describe('alks-cli', function(){
+    it('should write some tests', function(done){
+        done();
+    });
+});
