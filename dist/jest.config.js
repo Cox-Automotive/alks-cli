efe8605b292823ab31cf589370766c0fd39bde96
Ben Watson <ben.watson.developer@gmail.com>
2021-05-03 17:52:09 -0400

    configures jest

--- dist/jest.config.js
+++ dist/jest.config.js
@@ -111,9 +111,10 @@ exports.default = {
     //   "**/?(*.)+(spec|test).[tj]s?(x)"
     // ],
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    // testPathIgnorePatterns: [
-    //   "/node_modules/"
-    // ],
+    testPathIgnorePatterns: [
+        "/node_modules/",
+        "/dist/"
+    ],
     // The regexp pattern or array of patterns that Jest uses to detect test files
     // testRegex: [],
     // This option allows the use of a custom results processor
