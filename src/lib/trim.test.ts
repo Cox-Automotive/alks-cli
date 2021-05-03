cb05ed215f10b3ee8f2a743ded64f51c9104cd64
Ben Watson <ben.watson.developer@gmail.com>
2021-05-03 19:33:50 -0400

    adds tests for trim

--- src/lib/trim.test.ts
+++ src/lib/trim.test.ts
@@ -1,7 +1,51 @@
 import { trim } from './trim';
+
 describe('trim', () => {
-  it('should trim strings', () => {
-    const result = trim('   thing ');
-    expect(result).toEqual('thing');
-  });
+  interface TestCase {
+    description: string;
+    input: string;
+    result: ReturnType<typeof trim>;
+  }
+
+  const testCases: TestCase[] = [
+    {
+      description: 'when the input has no padding',
+      input: 'hello',
+      result: 'hello',
+    },
+    {
+      description: 'when the input is left-padded',
+      input: '  bob',
+      result: 'bob',
+    },
+    {
+      description: 'when the input is right-padded',
+      input: 'market  ',
+      result: 'market',
+    },
+    {
+      description: 'when the input is padded on both sides',
+      input: '    frisbee  ',
+      result: 'frisbee',
+    },
+    {
+      description: 'when the padding contains spaces, tabs, and newlines',
+      input: '\n\r \t  what\t \r\t\n',
+      result: 'what',
+    },
+  ];
+
+  for (const t of testCases) {
+    describe(t.description, () => {
+      let result: ReturnType<typeof trim>;
+
+      beforeEach(() => {
+        result = trim(t.input);
+      });
+
+      it('returns the correct result', () => {
+        expect(result).toEqual(t.result);
+      });
+    });
+  }
 });
