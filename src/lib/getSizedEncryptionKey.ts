55acc7a999ecaf9aaaa79f51a0fe796565e77bd7
Ben Watson <ben.watson.developer@gmail.com>
2021-05-24 12:53:05 -0400

    removes unused dependencies

--- src/lib/getSizedEncryptionKey.ts
+++ src/lib/getSizedEncryptionKey.ts
@@ -1,8 +1,6 @@
-import lp from 'left-pad';
-
 const ENC_LEN = 32;
 
 export function getSizedEncryptionKey(key: string) {
   // must be 256 bytes (32 characters)
-  return lp(key, ENC_LEN, 0).substring(0, ENC_LEN);
+  return key.padStart(ENC_LEN, '0').substring(0, ENC_LEN);
 }
