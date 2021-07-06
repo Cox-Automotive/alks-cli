d36c66180b5164ec40d6c6ee037f60248de333a6
Ben Watson <ben.watson.developer@gmail.com>
2021-07-06 11:34:16 -0400

    updates password prompt to check for and cache passwords

--- src/lib/promptForPassword.ts
+++ src/lib/promptForPassword.ts
@@ -1,9 +1,13 @@
 import { getPasswordFromPrompt } from './getPasswordFromPrompt';
-import { getPassword } from './state/password';
+import { getPassword, cachePassword } from './state/password';
 
 export async function promptForPassword(): Promise<string> {
   // Ignore failure since we're about to prompt for it
   const password = await getPassword().catch(() => undefined);
 
-  return getPasswordFromPrompt('Network Password', password);
+  const answer = await getPasswordFromPrompt('Network Password', password);
+
+  cachePassword(answer);
+
+  return answer;
 }
