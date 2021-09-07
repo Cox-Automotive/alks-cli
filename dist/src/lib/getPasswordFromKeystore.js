120b8270981d5c373f0791b4167bb1e8f8e1675c
Ben Watson <ben.watson.developer@gmail.com>
2021-09-07 11:15:05 -0400

    adds keytar dependency

--- dist/src/lib/getPasswordFromKeystore.js
+++ dist/src/lib/getPasswordFromKeystore.js
@@ -2,34 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPasswordFromKeystore = void 0;
 var tslib_1 = require("tslib");
-var underscore_1 = require("underscore");
-var isPasswordSecurelyStorable_1 = require("./isPasswordSecurelyStorable");
-var node_netrc_1 = tslib_1.__importDefault(require("node-netrc"));
 var getKeytar_1 = require("./getKeytar");
 var SERVICE = 'alkscli';
 var ALKS_USERID = 'alksuid';
 function getPasswordFromKeystore() {
     return tslib_1.__awaiter(this, void 0, void 0, function () {
-        var keytar, auth;
+        var keytar;
         return tslib_1.__generator(this, function (_a) {
             switch (_a.label) {
-                case 0:
-                    if (!isPasswordSecurelyStorable_1.isPasswordSecurelyStorable()) return [3 /*break*/, 3];
-                    return [4 /*yield*/, getKeytar_1.getKeytar()];
+                case 0: return [4 /*yield*/, getKeytar_1.getKeytar()];
                 case 1:
                     keytar = _a.sent();
-                    return [4 /*yield*/, keytar.getPassword(SERVICE, ALKS_USERID).catch(function () { return 'nopasswordfound'; })];
+                    return [4 /*yield*/, keytar.getPassword(SERVICE, ALKS_USERID).catch(function () { return null; })];
                 case 2: return [2 /*return*/, _a.sent()];
-                case 3:
-                    auth = node_netrc_1.default(SERVICE);
-                    if (!underscore_1.isEmpty(auth.password)) {
-                        return [2 /*return*/, auth.password];
-                    }
-                    else {
-                        return [2 /*return*/, null];
-                    }
-                    _a.label = 4;
-                case 4: return [2 /*return*/];
             }
         });
     });
--- dist/src/lib/storePassword.js
+++ dist/src/lib/storePassword.js
@@ -2,12 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.storePassword = void 0;
 var tslib_1 = require("tslib");
-var isPasswordSecurelyStorable_1 = require("./isPasswordSecurelyStorable");
 var log_1 = require("./log");
-var node_netrc_1 = tslib_1.__importDefault(require("node-netrc"));
-var chmod_1 = tslib_1.__importDefault(require("chmod"));
-var getFilePathInHome_1 = require("./getFilePathInHome");
-var getOwnerReadWriteOwnerPermission_1 = require("./getOwnerReadWriteOwnerPermission");
 var getKeytar_1 = require("./getKeytar");
 var SERVICE = 'alkscli';
 var ALKS_USERID = 'alksuid';
@@ -18,22 +13,13 @@ function storePassword(password) {
             switch (_a.label) {
                 case 0:
                     log_1.log('storing password');
-                    if (!isPasswordSecurelyStorable_1.isPasswordSecurelyStorable()) return [3 /*break*/, 3];
                     return [4 /*yield*/, getKeytar_1.getKeytar()];
                 case 1:
                     keytar = _a.sent();
                     return [4 /*yield*/, keytar.setPassword(SERVICE, ALKS_USERID, password)];
                 case 2:
                     _a.sent();
-                    return [3 /*break*/, 4];
-                case 3:
-                    node_netrc_1.default.update(SERVICE, {
-                        login: ALKS_USERID,
-                        password: password,
-                    });
-                    chmod_1.default(getFilePathInHome_1.getFilePathInHome('.netrc'), getOwnerReadWriteOwnerPermission_1.getOwnerReadWriteOnlyPermission());
-                    _a.label = 4;
-                case 4: return [2 /*return*/];
+                    return [2 /*return*/];
             }
         });
     });
