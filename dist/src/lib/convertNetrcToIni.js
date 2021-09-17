1ccd6dbf66ebe6f09e096d08b6cde29c6666a8d7
Ben Watson <ben.watson.developer@gmail.com>
2021-09-17 16:14:59 -0400

    updates netrc conversion to only remove the netrc file if it was only used by alks

--- dist/src/lib/convertNetrcToIni.js
+++ dist/src/lib/convertNetrcToIni.js
@@ -7,17 +7,20 @@ var path_1 = require("path");
 var os_1 = require("os");
 var credentials_1 = require("./state/credentials");
 var node_netrc_1 = tslib_1.__importDefault(require("node-netrc"));
+var log_1 = require("./log");
 var NETRC_FILE_PATH = path_1.join(os_1.homedir(), '.netrc');
-var ALKS_USERID = 'alksuid';
-var ALKS_TOKEN = 'alksclitoken';
+var NETRC_ALKS_PASSWORD = 'alkscli';
+var NETRC_ALKS_TOKEN = 'alksclitoken';
 function convertNetrcToIni() {
     return tslib_1.__awaiter(this, void 0, void 0, function () {
-        var netrcFileExists, credentialsFileExists, credentials, passwordAuth, tokenAuth;
+        var netrcFileExists, credentialsFileExists, credentials, passwordAuth, tokenAuth, netrcData, e_1;
         return tslib_1.__generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, promises_1.access(NETRC_FILE_PATH)
-                        .then(function () { return true; })
-                        .catch(function () { return false; })];
+                case 0:
+                    _a.trys.push([0, 8, , 9]);
+                    return [4 /*yield*/, promises_1.access(NETRC_FILE_PATH)
+                            .then(function () { return true; })
+                            .catch(function () { return false; })];
                 case 1:
                     netrcFileExists = _a.sent();
                     return [4 /*yield*/, promises_1.access(credentials_1.CREDENTIALS_FILE_PATH)
@@ -25,29 +28,41 @@ function convertNetrcToIni() {
                             .catch(function () { return false; })];
                 case 2:
                     credentialsFileExists = _a.sent();
-                    if (!(netrcFileExists && !credentialsFileExists)) return [3 /*break*/, 5];
+                    if (!(netrcFileExists && !credentialsFileExists)) return [3 /*break*/, 7];
                     return [4 /*yield*/, credentials_1.getCredentials()];
                 case 3:
                     credentials = _a.sent();
-                    passwordAuth = node_netrc_1.default(ALKS_USERID);
+                    passwordAuth = node_netrc_1.default(NETRC_ALKS_PASSWORD);
                     if (passwordAuth.password) {
                         credentials.password = passwordAuth.password;
+                        // remove the alks password from the netrc file
+                        node_netrc_1.default.update(NETRC_ALKS_PASSWORD);
                     }
-                    tokenAuth = node_netrc_1.default(ALKS_TOKEN);
+                    tokenAuth = node_netrc_1.default(NETRC_ALKS_TOKEN);
                     if (tokenAuth.password) {
                         credentials.token = tokenAuth.password;
+                        // remove the alks token from the netrc file
+                        node_netrc_1.default.update(NETRC_ALKS_PASSWORD);
                     }
                     return [4 /*yield*/, credentials_1.setCredentials(credentials)];
                 case 4:
                     _a.sent();
-                    _a.label = 5;
-                case 5:
                     if (!netrcFileExists) return [3 /*break*/, 7];
+                    return [4 /*yield*/, promises_1.readFile(NETRC_FILE_PATH, 'utf-8')];
+                case 5:
+                    netrcData = _a.sent();
+                    if (!(netrcData.trim().length === 0)) return [3 /*break*/, 7];
                     return [4 /*yield*/, promises_1.rm(NETRC_FILE_PATH)];
                 case 6:
                     _a.sent();
                     _a.label = 7;
-                case 7: return [2 /*return*/];
+                case 7: return [3 /*break*/, 9];
+                case 8:
+                    e_1 = _a.sent();
+                    // If the conversion fails, just pretend like the netrc file doesn't exist and continue anyway
+                    log_1.log('failed to convert netrc file to ini file: ' + e_1);
+                    return [3 /*break*/, 9];
+                case 9: return [2 /*return*/];
             }
         });
     });
--- src/lib/convertNetrcToIni.ts
+++ src/lib/convertNetrcToIni.ts
@@ -1,4 +1,4 @@
-import { access, rm } from 'fs/promises';
+import { access, rm, readFile } from 'fs/promises';
 import { join } from 'path';
 import { homedir } from 'os';
 import {
@@ -7,39 +7,52 @@ import {
   setCredentials,
 } from './state/credentials';
 import netrc from 'node-netrc';
+import { log } from './log';
 
 const NETRC_FILE_PATH = join(homedir(), '.netrc');
 
-const ALKS_USERID = 'alksuid';
-const ALKS_TOKEN = 'alksclitoken';
+const NETRC_ALKS_PASSWORD = 'alkscli';
+const NETRC_ALKS_TOKEN = 'alksclitoken';
 
 export async function convertNetrcToIni(): Promise<void> {
-  const netrcFileExists = await access(NETRC_FILE_PATH)
-    .then(() => true)
-    .catch(() => false);
-  const credentialsFileExists = await access(CREDENTIALS_FILE_PATH)
-    .then(() => true)
-    .catch(() => false);
+  try {
+    const netrcFileExists = await access(NETRC_FILE_PATH)
+      .then(() => true)
+      .catch(() => false);
+    const credentialsFileExists = await access(CREDENTIALS_FILE_PATH)
+      .then(() => true)
+      .catch(() => false);
 
-  // If credentials file hasn't been created yet but .netrc has, populate data from .netrc
-  if (netrcFileExists && !credentialsFileExists) {
-    const credentials = await getCredentials();
+    // If credentials file hasn't been created yet but .netrc has, populate data from .netrc
+    if (netrcFileExists && !credentialsFileExists) {
+      const credentials = await getCredentials();
 
-    const passwordAuth = netrc(ALKS_USERID);
-    if (passwordAuth.password) {
-      credentials.password = passwordAuth.password;
+      const passwordAuth = netrc(NETRC_ALKS_PASSWORD);
+      if (passwordAuth.password) {
+        credentials.password = passwordAuth.password;
+        // remove the alks password from the netrc file
+        netrc.update(NETRC_ALKS_PASSWORD);
+      }
+
+      const tokenAuth = netrc(NETRC_ALKS_TOKEN);
+      if (tokenAuth.password) {
+        credentials.token = tokenAuth.password;
+        // remove the alks token from the netrc file
+        netrc.update(NETRC_ALKS_PASSWORD);
+      }
+
+      await setCredentials(credentials);
+
+      // remove the old .netrc file for security reasons if all it had was ALKS info
+      if (netrcFileExists) {
+        const netrcData = await readFile(NETRC_FILE_PATH, 'utf-8');
+        if (netrcData.trim().length === 0) {
+          await rm(NETRC_FILE_PATH);
+        }
+      }
     }
-
-    const tokenAuth = netrc(ALKS_TOKEN);
-    if (tokenAuth.password) {
-      credentials.token = tokenAuth.password;
-    }
-
-    await setCredentials(credentials);
-  }
-
-  // remove the old .netrc file for security reasons
-  if (netrcFileExists) {
-    await rm(NETRC_FILE_PATH);
+  } catch (e) {
+    // If the conversion fails, just pretend like the netrc file doesn't exist and continue anyway
+    log('failed to convert netrc file to ini file: ' + e);
   }
 }
