5c160b24497a295ab7dfc48055544d44a82a6fe3
Ben Watson <ben.watson@coxautoinc.com>
2021-10-25 11:24:07 -0400

    fixes a bug with storing/retrieving the password

--- src/lib/getPasswordFromKeystore.ts
+++ src/lib/getPasswordFromKeystore.ts
@@ -3,12 +3,12 @@ import { log } from './log';
 import { getCredentials } from './state/credentials';
 
 const SERVICE = 'alkscli';
-const ALKS_USERID = 'alksuid';
+const ALKS_PASSWORD = 'alkspassword';
 
 export async function getPasswordFromKeystore(): Promise<string | undefined> {
   try {
     const keytar = await getKeytar();
-    return (await keytar.getPassword(SERVICE, ALKS_USERID)) ?? undefined;
+    return (await keytar.getPassword(SERVICE, ALKS_PASSWORD)) ?? undefined;
   } catch (e) {
     log((e as Error).message);
     log('Failed to use keychain. Checking for plaintext file');
--- src/lib/removePassword.ts
+++ src/lib/removePassword.ts
@@ -2,10 +2,10 @@ import { log } from './log';
 import { getKeytar } from './getKeytar';
 
 const SERVICE = 'alkscli';
-const ALKS_USERID = 'alksuid';
+const ALKS_PASSWORD = 'alkspassword';
 
 export async function removePassword(): Promise<void> {
   log('removing password');
   const keytar = await getKeytar();
-  keytar.deletePassword(SERVICE, ALKS_USERID);
+  keytar.deletePassword(SERVICE, ALKS_PASSWORD);
 }
