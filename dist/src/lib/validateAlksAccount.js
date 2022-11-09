29d609f1013ae158aed9a8631099cd45a5799f02
Ben Watson <ben.watson.developer@gmail.com>
2022-11-09 18:24:08 -0500

    adds support for alias in alks-developer-configure

--- dist/src/lib/validateAlksAccount.js
+++ dist/src/lib/validateAlksAccount.js
@@ -3,15 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.validateAlksAccount = void 0;
 var tslib_1 = require("tslib");
 var getAlksAccounts_1 = require("./getAlksAccounts");
+var getAwsAccountFromString_1 = require("./getAwsAccountFromString");
 function validateAlksAccount(account, role) {
     return tslib_1.__awaiter(this, void 0, void 0, function () {
-        var alksAccounts, matchingAccount;
+        var alksAccounts, awsAccount, matchingAccount;
         return tslib_1.__generator(this, function (_a) {
             switch (_a.label) {
                 case 0: return [4 /*yield*/, (0, getAlksAccounts_1.getAlksAccounts)()];
                 case 1:
                     alksAccounts = _a.sent();
-                    matchingAccount = alksAccounts.find(function (alksAccount) { return alksAccount.account == account && alksAccount.role == role; });
+                    return [4 /*yield*/, (0, getAwsAccountFromString_1.getAwsAccountFromString)(account)];
+                case 2:
+                    awsAccount = _a.sent();
+                    if (!awsAccount) {
+                        throw new Error("account: \"".concat(account, "\" could not be resolved to a valid AWS account"));
+                    }
+                    matchingAccount = alksAccounts.find(function (alksAccount) {
+                        return alksAccount.account.startsWith(awsAccount === null || awsAccount === void 0 ? void 0 : awsAccount.id) && alksAccount.role == role;
+                    });
                     if (!matchingAccount) {
                         throw new Error("account: \"".concat(account, "\" and role: \"").concat(role, "\" do not match any valid accounts"));
                     }
--- src/lib/validateAlksAccount.ts
+++ src/lib/validateAlksAccount.ts
@@ -1,4 +1,5 @@
 import { getAlksAccounts } from './getAlksAccounts';
+import { getAwsAccountFromString } from './getAwsAccountFromString';
 
 export async function validateAlksAccount(
   account: string,
@@ -6,8 +7,16 @@ export async function validateAlksAccount(
 ): Promise<void> {
   const alksAccounts = await getAlksAccounts();
 
+  const awsAccount = await getAwsAccountFromString(account);
+  if (!awsAccount) {
+    throw new Error(
+      `account: "${account}" could not be resolved to a valid AWS account`
+    );
+  }
+
   const matchingAccount = alksAccounts.find(
-    (alksAccount) => alksAccount.account == account && alksAccount.role == role
+    (alksAccount) =>
+      alksAccount.account.startsWith(awsAccount?.id) && alksAccount.role == role
   );
 
   if (!matchingAccount) {
