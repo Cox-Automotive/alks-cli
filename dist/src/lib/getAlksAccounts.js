c337f94b854292bd5bb03e818b4c0c63f8274dd1
Ben Watson <ben.watson.developer@gmail.com>
2022-11-09 19:25:55 -0500

    caches results of getAlksAccounts function for 5 seconds to improve performance

--- dist/src/lib/getAlksAccounts.js
+++ dist/src/lib/getAlksAccounts.js
@@ -5,14 +5,15 @@ var tslib_1 = require("tslib");
 var getAlks_1 = require("./getAlks");
 var getAuth_1 = require("./getAuth");
 var log_1 = require("./log");
-function getAlksAccounts(options) {
+var memoizee_1 = tslib_1.__importDefault(require("memoizee"));
+function _getAlksAccounts(options) {
     if (options === void 0) { options = {}; }
     return tslib_1.__awaiter(this, void 0, void 0, function () {
         var auth, alks, alksAccounts, filteredAlksAccounts;
         return tslib_1.__generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    (0, log_1.log)('retreiving alks account');
+                    (0, log_1.log)('refreshing alks accounts list');
                     return [4 /*yield*/, (0, getAuth_1.getAuth)()];
                 case 1:
                     auth = _a.sent();
@@ -26,6 +27,18 @@ function getAlksAccounts(options) {
                     filteredAlksAccounts = alksAccounts.filter(function (alksAccount) { return !options.iamOnly || alksAccount.iamKeyActive; });
                     return [2 /*return*/, filteredAlksAccounts];
             }
+        });
+    });
+}
+var memoized = (0, memoizee_1.default)(_getAlksAccounts, {
+    maxAge: 5000,
+});
+function getAlksAccounts(options) {
+    if (options === void 0) { options = {}; }
+    return tslib_1.__awaiter(this, void 0, void 0, function () {
+        return tslib_1.__generator(this, function (_a) {
+            (0, log_1.log)('retreiving alks accounts');
+            return [2 /*return*/, memoized(options)];
         });
     });
 }
