f88266d833525cc0d7caa24dca77bbac641a1585
Ben Watson <ben.watson.developer@gmail.com>
2023-10-10 12:32:29 -0400

    adds tests for getAwsCredentialsFile

--- dist/src/lib/getAwsCredentialsFile.test.js
+++ dist/src/lib/getAwsCredentialsFile.test.js
@@ -7,19 +7,56 @@ jest.mock('fs');
 jest.mock('./getFilePathInHome');
 describe('getAwsCredentialsFile', () => {
     const credPath = '/home/joebob/.aws';
+    const credFile = credPath + '/credentials';
     beforeEach(() => {
         getFilePathInHome_1.getFilePathInHome.mockReturnValue(credPath);
     });
     it('should return the cred file path', () => {
         fs_1.existsSync.mockImplementation((path) => {
             switch (path) {
-                case credPath: return true;
-                case credPath + '/credentials': return true;
-                default: return false;
+                case credPath:
+                    return true;
+                case credFile:
+                    return true;
+                default:
+                    return false;
             }
         });
         const file = (0, getAwsCredentialsFile_1.getAwsCredentialsFile)();
-        expect(file).toEqual(credPath + '/credentials');
+        expect(file).toEqual(credFile);
+    });
+    it('should make the cred file if it does not exist and return the cred file path', () => {
+        fs_1.existsSync.mockImplementation((path) => {
+            switch (path) {
+                case credPath:
+                    return true;
+                case credFile:
+                    return false;
+                default:
+                    return false;
+            }
+        });
+        const file = (0, getAwsCredentialsFile_1.getAwsCredentialsFile)();
+        expect(fs_1.openSync).toHaveBeenCalledWith(credFile, expect.any(String));
+        expect(fs_1.closeSync).toHaveBeenCalled();
+        expect(file).toEqual(credFile);
+    });
+    it('should make the cred file and folder if they do not exist and return the cred file path', () => {
+        fs_1.existsSync.mockImplementation((path) => {
+            switch (path) {
+                case credPath:
+                    return false;
+                case credFile:
+                    return false;
+                default:
+                    return false;
+            }
+        });
+        const file = (0, getAwsCredentialsFile_1.getAwsCredentialsFile)();
+        expect(fs_1.mkdirSync).toHaveBeenCalledWith(credPath);
+        expect(fs_1.openSync).toHaveBeenCalledWith(credFile, expect.any(String));
+        expect(fs_1.closeSync).toHaveBeenCalled();
+        expect(file).toEqual(credFile);
     });
 });
 //# sourceMappingURL=getAwsCredentialsFile.test.js.map
